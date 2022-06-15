41621888f214faec47dcf703ac980630aa35b98d
Vinícius Müller <vinigm.nho@gmail.com>
2022-06-15 00:23:49 -0300

    feat: add cpf, cep and cnpj keys and mouse support

--- keyboards/handwired/dactyl_manuform/5x6_5/keymaps/arcticlimer/keymap.c
+++ keyboards/handwired/dactyl_manuform/5x6_5/keymaps/arcticlimer/keymap.c
@@ -10,19 +10,25 @@
 #define MODRSFT MT(MOD_RSFT, KC_QUOT)
 #define MODRCTL MT(MOD_RCTL, KC_BSLASH)
 
+enum custom_keycodes {
+    GEN_CPF = SAFE_RANGE,
+    GEN_CEP,
+    GENCNPJ,
+};
+
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     [_QWERTY] = LAYOUT_5x6_5(
-        KC_ESC , KC_1  , KC_2  , KC_3  , KC_4  , KC_5  ,                         KC_6  , KC_7  , KC_8  , KC_9  , KC_0  ,KC_BSPC,
-        KC_TAB , KC_Q  , KC_W  , KC_E  , KC_R  , KC_T  ,                         KC_Y  , KC_U  , KC_I  , KC_O  , KC_P  ,KC_MINS,
-        KC_LSFT, KC_A  , KC_S  , KC_D  , KC_F  , KC_G  ,                         KC_H  , KC_J  , KC_K  , KC_L  ,KC_SCLN,MODRSFT,
-        KC_LCTL, KC_Z  , KC_X  , KC_C  , KC_V  , KC_B  ,                         KC_N  , KC_M  ,KC_COMM,KC_DOT ,KC_SLSH,MODRCTL,
+        KC_ESC , KC_1  , KC_2  , KC_3  , KC_4  , KC_5  ,                          KC_6 , KC_7  , KC_8  , KC_9  , KC_0  ,KC_BSPC,
+        KC_TAB , KC_Q  , KC_W  , KC_E  , KC_R  , KC_T  ,                          KC_Y , KC_U  , KC_I  , KC_O  , KC_P  ,KC_MINS,
+        KC_LSFT, KC_A  , KC_S  , KC_D  , KC_F  , KC_G  ,                          KC_H , KC_J  , KC_K  , KC_L  ,KC_SCLN,MODRSFT,
+        KC_LCTL, KC_Z  , KC_X  , KC_C  , KC_V  , KC_B  ,                          KC_N , KC_M  ,KC_COMM,KC_DOT ,KC_SLSH,MODRCTL,
                          KC_LBRC,KC_RBRC,RAISE ,KC_SPC,KC_GRV  ,          _______,KC_ENT,LOWER ,KC_PLUS,KC_EQL ,
                                                 _______,_______,          KC_LALT,KC_LGUI
     ),
 
     [_LOWER] = LAYOUT_5x6_5(
         KC_TILD,KC_EXLM, KC_AT ,KC_HASH,KC_DLR ,KC_PERC,                        KC_CIRC,KC_AMPR,KC_ASTR,KC_LPRN,KC_RPRN,KC_DEL,
-        _______,_______,_______,_______,_______,KC_LBRC,                        KC_RBRC, KC_P7 , KC_P8 , KC_P9 ,_______,KC_PLUS,
+        _______,_______,_______,KC_WH_D,KC_WH_U,KC_LBRC,                        KC_RBRC, KC_P7 , KC_P8 , KC_P9 ,_______,KC_PLUS,
         _______,KC_HOME,KC_PGUP,KC_PGDN,KC_END ,KC_LPRN,                        KC_RPRN, KC_P4 , KC_P5 , KC_P6 ,KC_MINS,KC_PIPE,
         _______,_______,KC_DEL ,_______,_______,_______,                        _______, KC_P1 , KC_P2 , KC_P3 ,KC_EQL ,KC_UNDS,
                         _______,KC_PSCR,_______,_______,_______,        _______,_______,_______,_______,KC_P0,
@@ -31,11 +37,38 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 
     [_RAISE] = LAYOUT_5x6_5(
           KC_F12 , KC_F1 , KC_F2 , KC_F3 , KC_F4 , KC_F5 ,                        KC_F6  , KC_F7 , KC_F8 , KC_F9 ,KC_F10 ,KC_F11 ,
-          _______,_______,_______,_______,_______,_______,                        _______,_______,_______,_______,_______,KC_MUTE,
+          _______,_______,_______,_______,_______,_______,                        GEN_CPF,GEN_CEP,GENCNPJ,_______,_______,KC_MUTE,
           _______,_______,_______,_______,_______,_______,                        KC_LEFT,KC_DOWN,KC_UP  ,KC_RGHT,_______,KC_VOLU,
-          _______,_______,_______,_______,_______,_______,                        _______,_______,_______,_______,_______,KC_VOLD,
-                          _______,_______,_______,_______,_______,        _______,_______,_______,_______,_______,
+          _______,_______,_______,_______,_______,_______,                        KC_MS_L,KC_MS_D,KC_MS_U,KC_MS_R,_______,KC_VOLD,
+                          _______,_______,_______,_______,_______,        _______,KC_BTN1,KC_BTN2,_______,_______,
                                                   _______,_______,        _______,_______
     )
 };
 
+bool process_record_user(uint16_t keycode, keyrecord_t *record) {
+    switch (keycode) {
+            case GEN_CPF:
+                if (record->event.pressed) {
+		    // Random cpf
+		    SEND_STRING("16021620429");
+                } else {
+                    // when keycode GEN_CPF is released
+                }
+                break;
+            case GEN_CEP:
+                if (record->event.pressed) {
+		    SEND_STRING("72306-527");
+                } else {
+                    // when keycode GEN_CEP is released
+                }
+                break;
+            case GENCNPJ:
+                if (record->event.pressed) {
+		    SEND_STRING("77.508.121/0001-73");
+                } else {
+                    // when keycode GENCNPJ is released
+                }
+                break;
+    }
+    return true;
+};
